d166d8f52411b2a76f3d5c1ad508b50090aaa27e
CRAZYShimakaze <674015283@qq.com>
2025-05-30 22:40:37 +0800

    1.修复命名大小写问题 2.修复命名相反问题 3.修复数量识别出错

--- yas-genshin/src/artifact/artifact.rs
+++ yas-genshin/src/artifact/artifact.rs
@@ -440,8 +440,8 @@ impl ArtifactSetName {
             "失冕的宝冠" | "褪光的翠尾" | "暗结的明花" | "举业的识刻" | "筹谋的共樽" => Some(ArtifactSetName::UnfinishedReverie),
             "魔战士的羽面" | "巡山客的信标" | "驯兽师的护符" | "秘术家的金盘" | "游学者的爪杯" => Some(ArtifactSetName::ScrollOfTheHeroOfCinderCity),
             "诸圣的礼冠" | "灵髓的根脉" | "异种的期许" | "夜域的迷思" | "纷争的前宴" => Some(ArtifactSetName::ObsidianCodex),
-            "深廊的遂失之冕" | "深廊的漫远之约" | "深廊的回奏之歌" | "深廊的湮落之刻" | "深廊的饫赐之宴" => Some(ArtifactSetName::LongNightsOath),
-            "被浸染的缨盔" | "夜鸣莺的尾羽" | "执灯人的誓词" | "不死者的哀铃" | "未吹响的号角" => Some(ArtifactSetName::FinaleOfTheDeepGalleries),
+            "深廊的遂失之冕" | "深廊的漫远之约" | "深廊的回奏之歌" | "深廊的湮落之刻" | "深廊的饫赐之宴" => Some(ArtifactSetName::FinaleOfTheDeepGalleries),
+            "被浸染的缨盔" | "夜鸣莺的尾羽" | "执灯人的誓词" | "不死者的哀铃" | "未吹响的号角" => Some(ArtifactSetName::LongNightsOath),
             _ => None,
         }
     }
